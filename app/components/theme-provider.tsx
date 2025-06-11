034aa33ebc577de7bce0b84e04b18a62f7f45d0d
Mahmoud <hotagx3@gmail.com>
2025-06-11 15:15:55 +0400

    final

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -1,9 +1,12 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ComponentProps } from "react";
+
+// Use ComponentProps to infer the props from NextThemesProvider
+type ThemeProviderProps = ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
