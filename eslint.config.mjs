1b7ab6b1cf6cced05f787489aa26b664f39df57d
Mahmoud <hotagx3@gmail.com>
2025-06-11 15:33:19 +0400

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),{
+    ignores: [
+      "app/generated/**/*",
+      "**/generated/**/*",
+      "**/*.generated.*",
+      ".next/**/*",
+      "out/**/*",
+      "dist/**/*",
+      "node_modules/**/*",
+    ],
+  },
 ];
 
 export default eslintConfig;
